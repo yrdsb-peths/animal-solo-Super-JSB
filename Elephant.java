e035a844865a58b0cc1dd98ba22f79ba647fce8f
Super-JSB <lujessie099@gmail.com>
2026-04-27 21:10:18 -0400

    add elephant and apple
    with move and setlocation methods. also called them in the myWorld

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Elephant class, our hero.
+ * 
+ * @author (Jessie Lu) 
+ * @version (April 27th, 2026)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
