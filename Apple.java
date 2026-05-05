cebcc65dec4a5216fb3668da485c0a639708555e
Super-JSB <lujessie099@gmail.com>
2026-05-05 16:02:48 -0400

    increase difficulty

--- Apple.java
+++ Apple.java
@@ -12,15 +12,24 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    int speed = 1;
+    
     public void act()
     {
-        setLocation(getX(), getY() + 1);
+        setLocation(getX(), getY() + speed);
         
         //Remove apple and draw game over when apple gets to bottom
         MyWorld world = (MyWorld) getWorld();
         if(getY() >= world.getHeight())
         {
             world.gameOver();
+            world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public int score = 0;
     Label scoreLabel;
+    int level = 1;
     public MyWorld() {
         super(600, 400, 1, false);
           
@@ -32,11 +33,17 @@ public class MyWorld extends World {
     {
         score++;
         scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level++;
+        }
     }
     //create a new apple at random location at the top of screen
     public void createApple()
     {
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
        
